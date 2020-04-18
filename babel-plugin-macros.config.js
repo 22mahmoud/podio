b46afe5a6cc4037b9bfb6d87eb0241416eaae750
22mahmoud <mahmoudss1997@gmail.com>
2020-04-18 08:02:42 +0200

    add tailwind.css

---
+++ babel-plugin-macros.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  twin: {
+    styled: 'styled-components',
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
