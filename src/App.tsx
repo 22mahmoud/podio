08104b85fedf697ea3877b8fdeeff4c3e9b757f0
22mahmoud <mahmoudss1997@gmail.com>
2020-04-18 08:53:24 +0200

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import tw from 'twin.macro';
 
-const App: React.FC<{}> = () => <div>Hello, World!</div>;
+const Container = tw.div`
+  w-screen h-screen
+  bg-gray-900
+  text-gray-200
+  flex flex-col
+  p-4
+  max-w-screen-md m-auto
+`;
+
+const App: React.FC<{}> = () => (
+  <Container>
+    Hello
+    {/* <Navbar />
+    <TracksList />
+  <Player /> */}
+  </Container>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+import tw from 'twin.macro';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './styles/index.css';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    ${tw`bg-gray-900`};
+  }
+`;
+
+const Main = () => (
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root'),
 );
